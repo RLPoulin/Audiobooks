d9c88bbe7a0b02514e6337a178416610d0391278
Robert Poulin <poulin.robert@gmail.com>
2020-01-25 16:46:35 -0500

    Removed useless code messing with sys.path from __init__.py

--- audiobooks/__init__.py
+++ audiobooks/__init__.py
@@ -1,13 +1,5 @@
 """My audiobook library program."""
 
-__author__ = "Robert Poulin"
+__author__ = "Robert Poulin <poulin.robert@gmail.com>"
 __license__ = "MIT"
 __version__ = "0.4.0"
-
-from pathlib import Path
-import sys
-
-
-package_folder: str = str(Path(__file__).resolve().parent.parent)
-if package_folder not in sys.path:
-    sys.path.insert(0, package_folder)
--- myfunctions/__init__.py
+++ myfunctions/__init__.py
@@ -5,13 +5,4 @@ __license__ = "MIT"
 __version__ = "2.1.3"
 __all__ = ["LogManager"]
 
-from pathlib import Path
-import sys
-
-
-package_folder: str = str(Path(__file__).resolve().parent.parent)
-if package_folder not in sys.path:
-    sys.path.insert(0, package_folder)
-
-
 from myfunctions.log_manager import LogManager
