3a56b6e792a449cbdd1c827418e0ec3778642f1e
Robert Poulin <poulin.robert@gmail.com>
2021-02-06 12:35:05 -0500

    refactor(myfunctions): improve code with flakehell

--- myfunctions/__init__.py
+++ myfunctions/__init__.py
@@ -1,8 +1,1 @@
 """Robert's module of small tools."""
-
-__authors__ = ["Robert Poulin (poulin.robert@gmail.com)"]
-__license__ = "MIT"
-__version__ = "2.1.3"
-__all__ = ["LogManager"]
-
-from myfunctions.log_manager import LogManager
