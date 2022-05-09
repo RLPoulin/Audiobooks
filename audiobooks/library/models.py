9759a744b500a37434dff1cfced00fa5231ddeed
Robert Poulin <poulin.robert@gmail.com>
2022-05-08 22:56:29 -0400

    feat(library.models): update models to the new audiobooks.database.Model base class

--- audiobooks/library/models.py
+++ audiobooks/library/models.py
@@ -1,35 +1,36 @@
 """Database table models for the audiobook library."""
 
 import datetime
+from typing import TypeVar
 
 from sqlalchemy.ext.hybrid import hybrid_property
 
+from audiobooks.database import Model
 from audiobooks.extensions import db
 
 from .utils import clean_name
 
+LibraryModelType = TypeVar("LibraryModelType", bound="LibraryModel")
 
-class LibraryModel(db.Model):
+
+class LibraryModel(Model):
     """Base class for a model containing only uniquely named items."""
 
     __abstract__ = True
-    key = db.Column(db.Integer, primary_key=True, autoincrement=True)
     _name = db.Column("name", db.String, unique=True, nullable=False)
+    date_added = db.Column(db.Date, default=datetime.date.today)
 
     def __init__(self, name: str, **kwargs) -> None:
-        """Construct a model instance.
-
-        Args:
-            name: name of the instance
-            kwargs: additional instance fields
-        """
-        self.name: str = name
+        super().__init__()
+        self.name = name
 
     def __repr__(self) -> str:
         return f"{self.__class__.__name__}('{self.name}')"
 
-    def __str__(self) -> str:
-        return self.name
+    @classmethod
+    def get_by_name(cls, name: str) -> LibraryModelType:
+        """Get a record by name."""
+        return cls.query.filter_by(name=clean_name(name)).first()
 
     @hybrid_property
     def name(self) -> str:
@@ -43,64 +44,53 @@ class LibraryModel(db.Model):
 
 
 class Author(LibraryModel):
-    """Defines the model for the ``authors`` table in the database."""
+    """Defines the model for the ``author`` table in the database."""
 
-    __tablename__ = "authors"
+    books = db.relationship("Book", backref="author", lazy=True)
+
+
+class Genre(LibraryModel):
+    """Defines the model for the ``genre`` table in the database."""
+
+    books = db.relationship("Book", backref="genre", lazy=True)
 
 
 class Series(LibraryModel):
     """Defines the model for the ``series`` table in the database."""
 
-    __tablename__ = "series"
-
-
-class Genre(LibraryModel):
-    """Defines the model for the ``genres`` table in the database."""
-
-    __tablename__ = "genres"
+    books = db.relationship("Book", backref="series", lazy=True)
 
 
 class Book(LibraryModel):
-    """Model for the ``books`` table in the database."""
+    """Model for the ``book`` table in the database."""
 
-    __tablename__ = "books"
-    author_key = db.Column(db.Integer, db.ForeignKey("authors.key"), nullable=False)
-    genre_key = db.Column(db.Integer, db.ForeignKey("genres.key"))
-    series_key = db.Column(db.Integer, db.ForeignKey("series.key"))
+    author_id = db.Column(db.Integer, db.ForeignKey("author.record_id"))
+    genre_id = db.Column(db.Integer, db.ForeignKey("genre.record_id"))
+    series_id = db.Column(db.Integer, db.ForeignKey("series.record_id"))
     release_date = db.Column(db.Date)
-    date_added = db.Column(db.Date)
-
-    author = db.relationship("Author", backref=__tablename__)
-    genre = db.relationship("Genre", backref=__tablename__)
-    series = db.relationship("Series", backref=__tablename__)
 
     def __init__(
         self,
         name: str,
-        author: Author,
         *,
-        genre: Genre | None = None,
-        series: Series | None = None,
-        release_date: datetime.date | None = None,
+        author: Author | str | None = None,
+        genre: Genre | str | None = None,
+        series: Series | str | None = None,
+        release_date: datetime.date | str | None = None,
     ) -> None:
-        """Construct a Book instance.
-
-        Args:
-            name: name of the book
-            author: instance object for the author
-            genre: instance object for the genre (optional)
-            series: instance object for the series (optional)
-            release_date: book release date (optional)
-        """
         super().__init__(name)
+        if isinstance(author, str):
+            author = Author.get_by_name(author)
+        if isinstance(genre, str):
+            author = Genre.get_by_name(genre)
+        if isinstance(series, str):
+            author = Series.get_by_name(series)
+        if isinstance(release_date, str):
+            release_date = datetime.date.fromisoformat(release_date)
         self.author = author
         self.genre = genre
         self.series = series
         self.release_date = release_date
-        self.date_added = datetime.date.today()
-
-    def __repr__(self) -> str:
-        return f"<Book('{self.name}', author='{self.author}')>"  # noqa
 
 
 # Dictionary associating book properties with the correct model.
