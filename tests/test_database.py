78697ff48768336b0112cdec45f1c8c24dc325c4
Robert Poulin <poulin.robert@gmail.com>
2022-05-22 18:10:41 -0400

    test(test_database): move repeated creation of examples to a fixture

--- tests/test_database.py
+++ tests/test_database.py
@@ -5,6 +5,7 @@ from __future__ import annotations
 from decimal import Decimal
 
 import flask_sqlalchemy
+import pytest
 
 from audiobooks.database import Model, SqliteDecimal, SupportDecimal, db
 
@@ -22,17 +23,21 @@ class ModelExample(Model):
         self.number = number
 
 
-def test_repr(test_db: flask_sqlalchemy.SQLAlchemy) -> None:
-    """Test for Model.__repr__ method."""
-    example = ModelExample.create(name="name")
+@pytest.fixture
+def example(test_db: flask_sqlalchemy.SQLAlchemy) -> ModelExample:
+    """Generate a test example."""
+    new_example = ModelExample.create(name="name")
     test_db.session.commit()
+    return new_example
+
+
+def test_repr(example: ModelExample) -> None:
+    """Test for Model.__repr__ method."""
     assert f"{example!r}" == "ModelExample(1)"
 
 
-def test_get_by_id(test_db: flask_sqlalchemy.SQLAlchemy) -> None:
+def test_get_by_id(example: ModelExample) -> None:
     """Test for Model.get_by_id method."""
-    example = ModelExample.create(name="name")
-    test_db.session.commit()
     assert ModelExample.get_by_id(example.record_id) is example
 
 
@@ -41,49 +46,39 @@ def test_get_by_id__failure() -> None:
     assert ModelExample.get_by_id(999999) is None
 
 
-def test_get(test_db: flask_sqlalchemy.SQLAlchemy) -> None:
+def test_get(example: ModelExample) -> None:
     """Test for Model.get method."""
-    example = ModelExample.create(name="name")
-    test_db.session.commit()
     assert ModelExample.get(example) is example
 
 
-def test_create(test_db: flask_sqlalchemy.SQLAlchemy) -> None:
+def test_create(example: ModelExample) -> None:
     """Test for Model.create method."""
-    example = ModelExample.create(name="name")
-    test_db.session.commit()
     assert ModelExample.get_by_id(example.record_id).name == "name"
 
 
-def test_update(test_db: flask_sqlalchemy.SQLAlchemy) -> None:
+def test_update(example: ModelExample, test_db: flask_sqlalchemy.SQLAlchemy) -> None:
     """Test for Model.update method."""
-    example = ModelExample.create(name="name")
-    test_db.session.commit()
     example.update(name="new_name")
     test_db.session.commit()
     assert ModelExample.get_by_id(example.record_id).name == "new_name"
 
 
-def test_delete(test_db: flask_sqlalchemy.SQLAlchemy) -> None:
+def test_delete(example: ModelExample, test_db: flask_sqlalchemy.SQLAlchemy) -> None:
     """Test for Model.delete method."""
-    example = ModelExample.create(name="name")
-    test_db.session.commit()
     example.delete()
     test_db.session.commit()
     assert ModelExample.get_by_id(example.record_id) is None
 
 
-def test_to_dict(test_db: flask_sqlalchemy.SQLAlchemy) -> None:
+def test_to_dict(example: ModelExample) -> None:
     """Test for Model.to_dict method."""
-    example = ModelExample.create(name="name")
-    test_db.session.commit()
     example_dict = ModelExample.get_by_id(example.record_id).to_dict()
     assert example_dict == {"record_id": 1, "name": "name", "number": None}
 
 
-def test_decimal(test_db: flask_sqlalchemy.SQLAlchemy) -> None:
+def test_decimal(example: ModelExample, test_db: flask_sqlalchemy.SQLAlchemy) -> None:
     """Test for SqliteDecimal."""
-    example = ModelExample.create(name="name", number=1.1)
+    example.update(number=1.1)
     test_db.session.commit()
     assert ModelExample.get_by_id(example.record_id).number == Decimal("1.1")
     assert ModelExample.query.filter(ModelExample.number > 1).first() == example
