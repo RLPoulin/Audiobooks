a881c62f072fa4cd4df9567be51d3906dc2bbcc6
Robert Poulin <poulin.robert@gmail.com>
2022-08-23 16:44:59 -0400

    test(conftest): move tests.configuration.TestConfig to tests.conftest

--- tests/conftest.py
+++ tests/conftest.py
@@ -7,16 +7,26 @@ import flask_sqlalchemy
 import pytest
 
 from audiobooks.app import create_app
+from audiobooks.configuration import Config
 from audiobooks.database import db
 
 
 os.environ["LOG_LEVEL"] = "CRITICAL"
 
 
+class TestConfig(Config):
+    """Configuration class for testing."""
+
+    ENV: str = "development"
+    SECRET_KEY: str = "no-secrets-in-tests"
+    TESTING: bool = True
+    SQLALCHEMY_DATABASE_URI: str = "sqlite:///:memory:"
+
+
 @pytest.fixture
 def app() -> flask.Flask:
     """Create an application for the tests."""
-    test_app: flask.Flask = create_app("tests.configuration.TestConfig")
+    test_app: flask.Flask = create_app("tests.conftest.TestConfig")
     test_app.logger.setLevel("CRITICAL")
     test_context = test_app.test_request_context()
     test_context.push()
