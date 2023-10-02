55307d48352c3b49f20ab82adaddd8532ba352ac
Robert Poulin <poulin.robert@gmail.com>
2023-10-02 16:30:52 -0400

    test: improve wording and optional return types

--- tests/conftest.py
+++ tests/conftest.py
@@ -24,24 +24,24 @@ class TestConfig(Config):
 
 
 @pytest.fixture()
-def app() -> flask.Flask:
+def app() -> flask.Flask:  # type: ignore[reportGeneralTypeIssue]
     """Create an application for the tests."""
     test_app: flask.Flask = create_app("tests.conftest.TestConfig")
     test_app.logger.setLevel("CRITICAL")
     test_context = test_app.test_request_context()
     test_context.push()
 
-    yield test_app
+    yield test_app  # type: ignore[reportGeneralTypeIssue]
     test_context.pop()
 
 
 @pytest.fixture()
-def test_db(app: flask.Flask) -> flask_sqlalchemy.SQLAlchemy:
+def test_db(app: flask.Flask) -> flask_sqlalchemy.SQLAlchemy:  # type: ignore[reportGeneralTypeIssue]
     """Create a database for the tests."""
-    db.app = app
+    db.app = app  # type: ignore[reportGeneralTypeIssue]
     with app.app_context():
         db.create_all()
 
-    yield db
+    yield db  # type: ignore[reportGeneralTypeIssue]
     db.session.close()
     db.drop_all()
--- tests/test_library_models.py
+++ tests/test_library_models.py
@@ -11,17 +11,17 @@ from audiobooks.library.models import Author, Book, date, get_library_item
 @pytest.fixture()
 def author(test_db: flask_sqlalchemy.SQLAlchemy) -> Author:
     """Generate an Author example."""
-    new_author = Author.create(name="Alice Bob")
+    test_author = Author.create(name="Alice Bob")
     test_db.session.commit()
-    return new_author
+    return test_author
 
 
 @pytest.fixture()
 def book(test_db: flask_sqlalchemy.SQLAlchemy) -> Book:
     """Generate an Author example."""
-    new_book = Book.create(name="Example", author="Alice Bob")
+    test_book = Book.create(name="Example", author="Alice Bob", series_number="1.1")
     test_db.session.commit()
-    return new_book
+    return test_book
 
 
 def test_author_repr(author: Author) -> None:
@@ -31,7 +31,9 @@ def test_author_repr(author: Author) -> None:
 
 def test_author_date_added(author: Author) -> None:
     """Test for Author.date_added column."""
-    assert Author.get_by_id(author.record_id).date_added == date.today()
+    db_item = Author.get_by_id(author.record_id)
+    assert db_item is not None
+    assert db_item.date_added == date.today()
 
 
 def test_author_get_by_name(author: Author) -> None:
@@ -55,22 +57,32 @@ def test_author_get(author: Author) -> None:
 
 
 def test_author_books(author: Author, book: Book) -> None:
-    """Test for Author.books column."""
-    assert Book.get_by_id(book.record_id).author is author
-    assert Author.get_by_id(author.record_id).books == [book]
+    """Test for Author.books Book.author relationship."""
+    db_book = Book.get_by_id(book.record_id)
+    assert db_book is not None
+    assert db_book.author is author
+    db_author = Author.get_by_id(author.record_id)
+    assert db_author is not None
+    assert db_author.books == [book]
 
 
 def test_book_create__default(test_db: flask_sqlalchemy.SQLAlchemy) -> None:
     """Test for Book.create method with default arguments."""
     book = Book.create(name="Example")
     test_db.session.commit()
-    assert Book.get_by_id(book.record_id).record_id == 1
-    assert Book.get_by_id(book.record_id).name == "Example"
-    assert Book.get_by_id(book.record_id).date_added == date.today()
-    assert Book.get_by_id(book.record_id).author is None
+    db_book = Book.get_by_id(book.record_id)
+    assert db_book is not None
+    assert db_book.record_id == 1
+    assert db_book.name == "Example"
+    assert db_book.author is None
+    assert db_book.genre is None
+    assert db_book.series is None
+    assert db_book.date_added == date.today()
+    assert db_book.series_number is None
+    assert db_book.release_date is None
 
 
-def test_book_create__object(
+def test_book_create__from_objects(
     author: Author, test_db: flask_sqlalchemy.SQLAlchemy
 ) -> None:
     """Test for Book.create method with object arguments."""
@@ -81,12 +93,14 @@ def test_book_create__object(
         release_date=date(2020, 10, 10),
     )
     test_db.session.commit()
-    assert Book.get_by_id(book.record_id).author is author
-    assert Book.get_by_id(book.record_id).series_number == Decimal("1.1")
-    assert Book.get_by_id(book.record_id).release_date == date(2020, 10, 10)
+    db_book = Book.get_by_id(book.record_id)
+    assert db_book is not None
+    assert db_book.author is author
+    assert db_book.series_number == Decimal("1.1")
+    assert db_book.release_date == date(2020, 10, 10)
 
 
-def test_book_create__string(
+def test_book_create__from_strings(
     author: Author, test_db: flask_sqlalchemy.SQLAlchemy
 ) -> None:
     """Test for Book.create method with string arguments."""
@@ -97,9 +111,11 @@ def test_book_create__string(
         release_date="2020-10-10",
     )
     test_db.session.commit()
-    assert Book.get_by_id(book.record_id).author is author
-    assert Book.get_by_id(book.record_id).series_number == Decimal("1.1")
-    assert Book.get_by_id(book.record_id).release_date == date(2020, 10, 10)
+    db_book = Book.get_by_id(book.record_id)
+    assert db_book is not None
+    assert db_book.author is author
+    assert db_book.series_number == Decimal("1.1")
+    assert db_book.release_date == date(2020, 10, 10)
 
 
 def test_get_library_item() -> None:
@@ -110,16 +126,18 @@ def test_get_library_item() -> None:
 
 def test_to_dict(author: Author, book: Book) -> None:
     """Test for Model.to_dict method."""
-    author_dict = Author.get_by_id(author.record_id).to_dict()
-    book_dict = Book.get_by_id(book.record_id).to_dict()
-    assert author_dict == {
+    db_author = Author.get_by_id(author.record_id)
+    assert db_author is not None
+    assert db_author.to_dict() == {
         "model": "Author",
         "record_id": 1,
         "books": ["Example"],
         "date_added": date.today().isoformat(),
         "name": "Alice Bob",
     }
-    assert book_dict == {
+    db_book = Book.get_by_id(book.record_id)
+    assert db_book is not None
+    assert db_book.to_dict() == {
         "model": "Book",
         "record_id": 1,
         "author": "Alice Bob",
@@ -128,5 +146,5 @@ def test_to_dict(author: Author, book: Book) -> None:
         "name": "Example",
         "release_date": None,
         "series": None,
-        "series_number": None,
+        "series_number": "1.1",
     }
